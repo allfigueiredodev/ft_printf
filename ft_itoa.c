1d90009b2c372b0d433541c990ace4f711cb5a3c
Allesson Figueiredo <allfigueiredo@outlook.com>
2023-06-25 21:44:31 -0300

    %x and %X conversions done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,25 +6,28 @@
 /*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:58:48 by aperis-p          #+#    #+#             */
-/*   Updated: 2023/06/25 20:41:20 by aperis-p         ###   ########.fr       */
+/*   Updated: 2023/06/25 21:31:14 by aperis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_itoa(unsigned long n, int base)
+int	ft_itoa(unsigned long n, int base, int caps)
 {
 	int		count;
 	char	*hexa_chrs;
 
 	count = 0;
-	hexa_chrs = "0123456789abcdef";
+	if (caps)
+		hexa_chrs = "0123456789ABCDEF";
+	else
+		hexa_chrs = "0123456789abcdef";
 	if (n < (unsigned long)base)
 		return ft_putchar(hexa_chrs[n]);
 	else
 	{
-		count = ft_itoa(n / base, base);
-		return count + ft_itoa(n % base, base);
+		count = ft_itoa(n / base, base, caps);
+		return count + ft_itoa(n % base, base, caps);
 	}
 }
 
@@ -34,6 +37,6 @@ int hash_printer (unsigned long n, int base)
 		ft_putstr("0x");
 	else if (base == 8)
 		ft_putstr("0");
-	return ft_itoa(n, base) + 2;
+	return ft_itoa(n, base, 0) + 2;
 }
 
